1f2d9eac72071529df803bb4f3638cec0de19171
Pedro Henrique <phenrique.camara@hotmail.com>
2022-08-30 15:44:52 -0300

    Colocando valores em formato de dinheiro

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,26 @@
 const button = document.getElementsByTagName('button')[0]
-const dollar = 5.2
+const dollarValue = 5.2
 
 const convertValues = () => {
     const input = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
     
-    const conversion = input / dollar
+    const conversion = input / dollarValue
+
+    const realFormat = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(input)    
+
+    const  dollarFormat = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    }).format(conversion)    
+
     
-    realValueText.innerHTML = input
-    currencyValueText.innerHTML = conversion
+    realValueText.innerHTML = realFormat
+    currencyValueText.innerHTML = dollarFormat
 }
 
 button.addEventListener('click', convertValues)
