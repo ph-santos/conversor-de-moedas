df921eebc471d0b387f318046a805e7c39aba71f
Pedro Henrique <phenrique.camara@hotmail.com>
2022-08-30 15:15:40 -0300

    Colocando o innerHTML

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,13 @@ const dollar = 5.2
 
 const convertValues = () => {
     const input = document.getElementById('input-real').value
-
-    const conversion = input * dollar
-
-    console.log(conversion)
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValueText = document.getElementById('currency-value-text')
+    
+    const conversion = input / dollar
+    
+    realValueText.innerHTML = input
+    currencyValueText.innerHTML = conversion
 }
 
 button.addEventListener('click', convertValues)
